3f0892ee183125337b8b5fe65be67f67c053ab23
Nurlis03 <kimbiletovnurlis03@gmail.com>
2024-04-10 13:27:09 +0600

    added comments to 'CardStatusServiceClient'

--- microservice1/src/main/java/org/acme/service/CardStatusServiceClient.java
+++ microservice1/src/main/java/org/acme/service/CardStatusServiceClient.java
@@ -5,16 +5,16 @@ import jakarta.ws.rs.core.MediaType;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 // TODO: check api best practice
-@Path("/microservice-2") // specify base path to microservice 2: http://localhost:9090/microservice-2
+@Path("/microservice-2") // specify base path to microservice 2: http://localhost:2222/microservice-2 local profile
 @RegisterRestClient(configKey = "microservice2-api")
 @Consumes(MediaType.APPLICATION_XML)
 @Produces(MediaType.APPLICATION_XML)
 public interface CardStatusServiceClient {
     @POST
-    @Path("/set-card-status") // add path for set card status: http://localhost:9090/microservice-2/set-card-status
+    @Path("/set-card-status") // add path for set card status: http://localhost:2222/microservice-2/set-card-status local profile
     String setCardStatus(String requestBody);
 
     @GET
-    @Path("/get-card-status") // add path for get card status: http://localhost:9090/microservice-2/get-card-status
+    @Path("/get-card-status") // add path for get card status: http://localhost:2222/microservice-2/get-card-status local profile
     String getCardStatus(String requestBody);
 }
